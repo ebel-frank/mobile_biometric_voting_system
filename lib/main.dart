1e36d19cdf9eb9a5963338ba89224ee91dcd11b6
ebel-frank <frankebeledike@gmail.com>
2024-10-02 16:13:04 +0100

    hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mbvs/common/color.dart';
 import 'package:mbvs/states/data_provider.dart';
